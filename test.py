5144ac5a6378537a6227afa2809b1a9a9e577f42
jonatan <jonaengs@gmail.com>
2020-09-21 15:17:14 +0200

    create activities

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import os
 import sys
 from collections import namedtuple
 from datetime import datetime
+from pprint import pprint
 
 max_line_size = 114  # max line size i 000s første fil iaf
 data_offset = 6
@@ -16,10 +17,10 @@ trackpoint_data_indices = [0, 1, 3, 5, 6]
 max_file_size_limit = max_line_size * max_lines_limit
 is_below_max_size = lambda fp: os.stat(fp).st_size < max_file_size_limit
 
-Activity = namedtuple("Activity", ("transportation_mode", "start_date_time", "end_date_time"))
-Trackpoint = namedtuple("Trackpoint", ("latitude", "longtitude", "altitude", "datetime"))
-Label = namedtuple("Label", ("start_time", "end_time", "mode"))
 User = namedtuple("User", ("id", "has_labels"))
+Trackpoint = namedtuple("Trackpoint", ("latitude", "longtitude", "altitude", "datetime"))
+Label = namedtuple("Label", ("start_datetime", "end_datetime", "mode"))
+Activity = namedtuple("Activity", ("start_date_time", "end_date_time", "transportation_mode", "trackpoints"))
 
 def create_trackpoint(lat, longt, alt, date, time):
     return Trackpoint(float(lat), float(longt), int(alt), parse_date_time(date, time))
@@ -27,25 +28,48 @@ def create_trackpoint(lat, longt, alt, date, time):
 def create_label(start_date, start_time, end_date, end_time, activity):
     return Label(parse_date_time(start_date, start_time), parse_date_time(end_date, end_time), activity)
 
+def create_activity(start_date, start_time, end_date, end_time, activity):
+    return Activity(parse_date_time(start_date, start_time), parse_date_time(end_date, end_time), activity, [])
+
+def activity_from_trackpoints(first, last):
+    return Activity(first.datetime, last.datetime, "N/A", [])
+
+def trackpoint_from_line(line):
+    data = line.strip().split(",")
+    return create_trackpoint(*(data[i] for i in trackpoint_data_indices))
+
 users = [User(i, False) for i in range(num_users)]
 with open("dataset/labeled_ids.txt", mode="r") as labels_file:
     for uid in map(int, labels_file.readlines()):
         users[uid] = User(uid, True)
 
-for user in users:
+for user in users[0:1]:
     print(user)
     trackpoints_path = get_trackpoints_path(user.id)
     trackpoint_files = os.listdir(trackpoints_path)
     if user.has_labels:
         labels_file_path = get_labels_path(user.id)
         with open(labels_file_path) as labels_file:
-            labels = map(create_label, labels_file.readlines()[1:])
-    trackpoints = []
+            activities = {(activity.start_datetime): activity for activity in map(create_activity, labels_file.readlines()[1:])}
+    else:
+        activities = {}
     for file_path in filter(is_below_max_size, (trackpoints_path + fn for fn in trackpoint_files)):
         with open(file_path) as f:
-            for line in f.readlines()[data_offset:]:
-                data = line.strip().split(",")
-                trackpoints.append(create_trackpoint(*(data[i] for i in trackpoint_data_indices)))
+            lines = f.readlines()[data_offset:]
+            first_trackpoint = trackpoint_from_line(lines[0])
+            trackpoints = None
+            if user.has_labels:
+                activity = activities.get((first_trackpoint.datetime))
+                if activity:
+                    trackpoints = activity.trackpoints
+            if trackpoints is None:
+                activity = activity_from_trackpoints(first_trackpoint, trackpoint_from_line(lines[-1]))
+                activities[activity.start_date_time] = activity
+                trackpoints = activity.trackpoints
+            trackpoints += lines
+
+
+    pprint(activities.keys())
 
 
 
