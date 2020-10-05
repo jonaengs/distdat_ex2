f5e5bbe8f80df8f039264aa85748d1189a1d3d5c
magnus <magnurr@stud.ntnu.no>
2020-10-05 11:21:36 +0200

    Add query 7

--- src/queries.py
+++ src/queries.py
@@ -3,6 +3,8 @@ from tabulate import tabulate
 from tables_metadata import all_table_names
 
 # with DbConnector as cursor
+
+
 class QueryRunner:
     @staticmethod
     def query_printer(query_number, rows, column_names):
@@ -11,26 +13,31 @@ class QueryRunner:
         print("\n")
 
     def query_1(self):
-        table_name_mapping = {"User": "users", "Activity": "activities", "TrackPoint": "trackpoints"}
-        
+        table_name_mapping = {
+            "User": "users", "Activity": "activities", "TrackPoint": "trackpoints"}
+
         for table_name in all_table_names:
-            query = "SELECT COUNT(*) AS Number_of_%s FROM %s" # Kanskje bytt ut * med ID elns
-            self.cursor.execute(query % (table_name_mapping[table_name], table_name))
+            # Kanskje bytt ut * med ID elns
+            query = "SELECT COUNT(*) AS Number_of_%s FROM %s"
+            self.cursor.execute(
+                query % (table_name_mapping[table_name], table_name))
             rows = self.cursor.fetchall()
 
-            self.query_printer(query_number=1, rows=rows, column_names=self.cursor.column_names)
-    
+            self.query_printer(query_number=1, rows=rows,
+                               column_names=self.cursor.column_names)
+
     def query_2(self):
         # Find the average number of activities per user.
         query = """SELECT AVG(Number_of_activities) AS Average_activities_per_user FROM (
                         SELECT user_id, Count(user_id) as Number_of_activities
                         FROM Activity
                         GROUP BY user_id
-                    ) AS Number_of_activities;""" 
+                    ) AS Number_of_activities;"""
         self.cursor.execute(query)
         rows = self.cursor.fetchall()
 
-        self.query_printer(query_number=2, rows=rows, column_names=self.cursor.column_names)
+        self.query_printer(query_number=2, rows=rows,
+                           column_names=self.cursor.column_names)
 
     def query_3(self):
         # Find the top 20 users with the highest number of activities
@@ -42,8 +49,9 @@ class QueryRunner:
         self.cursor.execute(query)
         rows = self.cursor.fetchall()
 
-        self.query_printer(query_number=3, rows=rows, column_names=self.cursor.column_names)
-    
+        self.query_printer(query_number=3, rows=rows,
+                           column_names=self.cursor.column_names)
+
     def query_4(self):
         # Find all users who have taken a taxi.
 
@@ -55,7 +63,8 @@ class QueryRunner:
         self.cursor.execute(query)
         rows = self.cursor.fetchall()
 
-        self.query_printer(query_number=4, rows=rows, column_names=self.cursor.column_names)
+        self.query_printer(query_number=4, rows=rows,
+                           column_names=self.cursor.column_names)
 
     def query_5(self):
         # Find the top 20 users with the highest number of activities
@@ -69,7 +78,8 @@ class QueryRunner:
         self.cursor.execute(query)
         rows = self.cursor.fetchall()
 
-        self.query_printer(query_number=5, rows=rows, column_names=self.cursor.column_names)
+        self.query_printer(query_number=5, rows=rows,
+                           column_names=self.cursor.column_names)
 
     def query_6(self):
         # a) Find the year with the most activities.
@@ -85,7 +95,8 @@ class QueryRunner:
         self.cursor.execute(query_a)
         rows_a = self.cursor.fetchall()
 
-        self.query_printer(query_number="6a", rows=rows_a, column_names=self.cursor.column_names)
+        self.query_printer(query_number="6a", rows=rows_a,
+                           column_names=self.cursor.column_names)
 
         # b) Is this also the year with most recorded hours?
 
@@ -100,16 +111,40 @@ class QueryRunner:
         self.cursor.execute(query_b)
         rows_b = self.cursor.fetchall()
 
-        self.query_printer(query_number="6b", rows=rows_b, column_names=self.cursor.column_names)
+        self.query_printer(query_number="6b", rows=rows_b,
+                           column_names=self.cursor.column_names)
 
         # (2008 != 2000)
         is_OR_is_not = "IS" if rows_a[0][0] == rows_b[0][0] else "is NOT"
-        print("6b) The year with most activities", is_OR_is_not, "the year with most recorded hours")
-    
+        print("6b) The year with most activities",
+              is_OR_is_not, "the year with most recorded hours")
+
+    def query_7(self):
+        # a) Find the year with the most activities.
+
+        query = """SELECT
+                        Activity.user_id,
+                        SUM(ST_Distance_Sphere(
+                            ST_GeomFromText(ST_AsText(Point(From_points.lon, From_points.lat))),
+                            ST_GeomFromText(ST_AsText(Point(To_points.lon, To_points.lat))))) AS Total_distance
+                    FROM TrackPoint AS From_points
+                        INNER JOIN TrackPoint AS To_points
+                        ON From_points.id = To_points.id - 1
+                        INNER JOIN Activity
+                        ON From_points.activity_id = Activity.id
+                    WHERE Activity.user_id = 112;
+                ;"""
+
+        self.cursor.execute(query)
+        rows = self.cursor.fetchall()
+
+        self.query_printer(query_number="7", rows=rows,
+                           column_names=self.cursor.column_names)
+
     def query_10(self):
         # Find the users who have tracked an activity in the Forbidden City of Beijing (lat 39.916, lon 116.397)
         # Exaclty lat=39.916 and lon=116.397 gives no answer, so we rounded the values to two decimals and made a range
-        
+
         query = """
                     SELECT DISTINCT user_id
                     FROM Activity as A
@@ -126,7 +161,8 @@ class QueryRunner:
         self.cursor.execute(query)
         rows = self.cursor.fetchall()
 
-        self.query_printer(query_number=10, rows=rows, column_names=self.cursor.column_names)
+        self.query_printer(query_number=10, rows=rows,
+                           column_names=self.cursor.column_names)
 
 
 def main():
